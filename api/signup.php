801246d34a7b523527c1660c22dc61fd4defd722
Samuel Kostadinov <SamuelKostadinovGit@gmail.com>
2021-04-29 11:04:47 +0200

    Fatto cose

--- api/signup.php
+++ api/signup.php
@@ -5,12 +5,12 @@ require_once "../libs/helper.php";
 
 session_start();
 
-$username = $_POST['username'];
-$name = $_POST['name'];
-$surname = $_POST['surname'];
+$username = cleanInput($_POST['username']);
+$name = cleanInput($_POST['name']);
+$surname = cleanInput($_POST['surname']);
 $mail = cleanInput($_POST['mail'], true);
-$password = $_POST['password'];
-$rePassword = $_POST['rePassword'];
+$password = cleanInput($_POST['password']);
+$rePassword = cleanInput($_POST['rePassword']);
 $toRedirect = urldecode($_POST['redirect'] ?? urlencode('/?r=home'));
 
 $DBaccess = new DBaccess();
@@ -18,7 +18,7 @@ $DBaccess = new DBaccess();
 $existingUsername = ($DBaccess-> getConnection() !== false) ? $DBaccess->getUsernameQuery($username) : false;
 $existingMail = ($DBaccess-> getConnection() !== false) ? $DBaccess->getMailQuery($username) : false;
 
-if(!$existingUsername && !$existingMail && $password == $rePassword){
+if(!$existingUsername && !$existingMail && $password == $rePassword && $mail != "Fatal error"){
 
     $insertResult = $DBaccess-> insertUser($username, $mail, $name, $surname, hash('sha512', $password));
     $DBaccess->closeConnection();
--- libs/helper.php
+++ libs/helper.php
@@ -10,18 +10,6 @@ function handleClosureOfQuery(string $data): string{
 
     return $data;
 }
-/*
-function handleCommenting(stirng $data):string{
-
-    return $data;
-}
-
-function handleInjection(string $data, bool $withoutNumbers): string{
-
-
-
-    return $data;
-}*/
 
 function cleanInput(string $data, bool $testForMail =false): string{
 
@@ -43,24 +31,23 @@ function cleanInput(string $data, bool $testForMail =false): string{
         $data = filter_var($data, FILTER_SANITIZE_EMAIL);
         $at = PcreRegex::match("/@/", $data);
         $dot = PcreRegex::match("/./", $data);
-        if(!$at || $dot){
+        if(!$at || !$dot){
 
             $sanitazable = false;
         }
     }
 
-    /*
+
     if(!$sanitazable){
 
         return "Fatal error";
     }
-    */
 
     //clean for SQL injection
     //even if it was unneccessary due to the use of base64
-    /*$testForSQLInjectionNumbers = "/(?i)OR \d+=\d+/";
-    $testForSQLInjection = "/(?i)OR '(\w+)?'='(\w+)?'/";
-    $testForCommentingSQL = "/--/";*/
+    //$testForSQLInjectionNumbers = "/(?i)OR \d+=\d+/";
+    //$testForSQLInjection = "/(?i)OR '(\w+)?'='(\w+)?'/";
+    //$testForCommentingSQL = "/--/";
     $testForPrematureClosureOfQuery = "/'/";
 
     /*$commenting = PcreRegex::match($testForCommentingSQL, $data);
@@ -72,17 +59,6 @@ function cleanInput(string $data, bool $testForMail =false): string{
 
         $data = handleClosureOfQuery($data);
     }
-
-    /*if($commenting){
-
-        $data = handleCommenting($data);
-    }
-
-    if($injecting || $injectingNumbers){
-
-        $data = handleInjection($data, $injecting);
-    }*/
-
 
     return $data;
 }
