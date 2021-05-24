af8db14cc3092f1005d2770826bb0711122852a9
marcotesser <marco.tesser.1@studenti.unipd.it>
2021-05-24 17:36:57 +0200

    prova 118

--- js/menu.js
+++ js/menu.js
@@ -14,19 +14,22 @@ window.addEventListener('resize',function(event){
 
 function openmenu(){
 
-    document.getElementById('menuhamburger').classList.add('nascosto');
-    document.getElementById('menuhamburger').classList.remove('float');
-    document.getElementById('chiudimenu').classList.remove('nascosto');
-    document.getElementById('chiudimenu').classList.add('float');
-    document.getElementById('menu').classList.remove('nascosto');
-
+    if(window.innerWidth < 640){
+        document.getElementById('menuhamburger').classList.add('nascosto');
+        document.getElementById('menuhamburger').classList.remove('float');
+        document.getElementById('chiudimenu').classList.remove('nascosto');
+        document.getElementById('chiudimenu').classList.add('float');
+        document.getElementById('menu').classList.remove('nascosto');
+    }
 }
 
 function closemenu(){
 
-    document.getElementById('menuhamburger').classList.remove('nascosto');
-    document.getElementById('menuhamburger').classList.add('float');
-    document.getElementById('chiudimenu').classList.add('nascosto');
-    document.getElementById('chiudimenu').classList.remove('float');
-    document.getElementById('menu').classList.add('nascosto');
+    if(window.innerWidth < 640){
+        document.getElementById('menuhamburger').classList.remove('nascosto');
+        document.getElementById('menuhamburger').classList.add('float');
+        document.getElementById('chiudimenu').classList.add('nascosto');
+        document.getElementById('chiudimenu').classList.remove('float');
+        document.getElementById('menu').classList.add('nascosto');
+    }
 }
