8f1c83a41d4bf6c09dfeda07e6f372535d0a7449
Emma Roveroni <emma.roveroni@studenti.unipd.it>
2021-05-26 18:42:58 +0200

    eh

--- js/menu.js
+++ js/menu.js
@@ -1,33 +1,35 @@
-function loadmenu(){
-
-    if(window.innerWidth > 640){
-
-        document.getElementById('menu').classList.remove('nascosto');
-        document.getElementById('menuhamburger').classList.add('nascosto');
-        document.getElementById('menu').classList.add('floatleft');
-        document.getElementById('menuhamburger').classList.remove('float')    
-        return;
-    }
-    document.getElementById('menu').classList.add('nascosto');
-    document.getElementById('menuhamburger').classList.remove('nascosto');
-    document.getElementById('menu').classList.remove('floatleft');
-    document.getElementById('menuhamburger').classList.add('float');
+function loadmenu() {
+  if (window.innerWidth > 640) {
+    document.getElementById("menu").classList.remove("nascosto");
+    document.getElementById("menuhamburger").classList.add("nascosto");
+    document.getElementById("menu").classList.add("floatleft");
+    document.getElementById("menuhamburger").classList.remove("float");
+    return;
+  }
+  document.getElementById("menu").classList.add("nascosto");
+  document.getElementById("menuhamburger").classList.remove("nascosto");
+  document.getElementById("menu").classList.remove("floatleft");
+  document.getElementById("menuhamburger").classList.add("float");
 }
-window.addEventListener('resize',function(event){
-    loadmenu();
-})
-function openmenu(){
-        document.getElementById('menuhamburger').classList.add('nascosto');
-        document.getElementById('menuhamburger').classList.remove('float');
-        document.getElementById('chiudimenu').classList.remove('nascosto');
-        document.getElementById('chiudimenu').classList.add('float');
-        document.getElementById('menu').classList.remove('nascosto');
+window.addEventListener("resize", function (event) {
+  loadmenu();
+});
+function openmenu() {
+  document.getElementById("menuhamburger").classList.add("nascosto");
+  document.getElementById("menuhamburger").classList.remove("float");
+  document.getElementById("chiudimenu").classList.remove("nascosto");
+  document.getElementById("chiudimenu").classList.add("float");
+  document.getElementById("menu").classList.remove("nascosto");
 }
 
-function closemenu(){
-        document.getElementById('menuhamburger').classList.remove('nascosto');
-        document.getElementById('menuhamburger').classList.add('float');
-        document.getElementById('chiudimenu').classList.add('nascosto');
-        document.getElementById('chiudimenu').classList.remove('float');
-        document.getElementById('menu').classList.add('nascosto');
+function closemenu() {
+  document.getElementById("menuhamburger").classList.remove("nascosto");
+  document.getElementById("menuhamburger").classList.add("float");
+  document.getElementById("chiudimenu").classList.add("nascosto");
+  document.getElementById("chiudimenu").classList.remove("float");
+  document.getElementById("menu").classList.add("nascosto");
 }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  loadmenu();
+});
